35055fa73322cde46e68fdbe38ab4af03614dd83
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2022-12-19 10:52:06 +0100

    imported express module

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// import express module into the package:
+const express = require('express');
+// declare variable that encapsulates Express functionality to configure web server:
+// variable then used to route HTTP requests and responses
+const app = express();
+
+
