432ccc68ab27f64489a1aad9b5bcbed6fc69db3f
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2022-12-29 13:29:13 +0100

    created movieSchema and userSchema

---
+++ models.js
@@ -0,0 +1,34 @@
+const { default: mongoose } = require('mongoose');
+const mogoose = require('mongoose');
+
+let movieSchema = mogooose.Schema({ 
+    Title: {type: String, required: true},
+    Description: { type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String,
+        YearofBirth: String,
+        YeardofDeath: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
