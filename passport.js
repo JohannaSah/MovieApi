0b1759b807710de4a8e4f4595fd9d50373936c9b
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-01-17 11:39:21 +0100

    callback to validate entered password

--- passport.js
+++ passport.js
@@ -22,6 +22,10 @@ passport.use(new LocalStrategy({
                 console.log('incorrect username');
                 return callback(null, false, {message: 'Incorrect username or password'});
             }
+            if(!user.validatePassword(password)) {
+                console.log('incorrect password');
+                return callback(null, false, {message: 'Incorrect password'};)
+            }
 
             console.log.apply('finished');
             return callback(null, user);
