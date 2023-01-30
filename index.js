45bdcf04d77712efc54c17982cfb18f5767c1a72
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-01-30 09:16:15 +0100

    connected to database on mongo atlas

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ let auth = require('./auth')(app); // (app) ensures, that Express is available i
 const passport = require('passport'); // require passport module
 require('./passport'); // import passport.js file
 
-mongoose.connect('mongodb://localhost:27017/myFlixDataBase', {useNewUrlParser: true, useUnifiedTopology: true}).then( () => console.log('database is connected'));
+// mongoose.connect('mongodb://localhost:27017/myFlixDataBase', {useNewUrlParser: true, useUnifiedTopology: true}).then( () => console.log('database is connected'));
+mongoose.connect('mongodb+srv://myFlixDBadmin:9ZXdbmMo28eA@cluster0.bjuijqy.mongodb.net/myFlixDataBase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then( () => console.log('database is connected'));
+
 
 // Create 
 
