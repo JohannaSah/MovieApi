f339d559a7014d57637de82a30d4ba040ed64908
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-02-01 13:58:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use(morgan('common'));
 
 // mongoose.connect('mongodb://localhost:27017/myFlixDataBase', {useNewUrlParser: true, useUnifiedTopology: true}).then( () => console.log('database is connected'));
 // mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb+srv://myFlixDBadmin:9ZXdbmMo28eA@cluster0.bjuijqy.mongodb.net/myFlixMovieDataBase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://myFlixDBadmin:9ZXdbmMo28eA@cluster0.bjuijqy.mongodb.net/myFlixMovieDataBase?retryWrites=true&w=majority');
 
 
 // Create 
