9bb1d749b206dd29e8598cc3f1ec892b2cbdb925
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-02-20 18:16:36 +0100

    test2

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ app.post('/users',
     }
 
     let hashedPassword = Users.hashPassword(req.body.Password);
+
     Users.findOne({ Username: req.body.Username })
         .then((user) => {
             if (user) {
@@ -205,12 +206,15 @@ app.put('/users/:Username', passport.authenticate('jwt', {session: false}),
     if(!errors.isEmpty()) {
         return res.status(422).json({errors: errors.array()});
     }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+
     Users.findOneAndUpdate(
         { Username: req.params.Username },
         {
             $set: {
                 Username: req.body.Username,
-                Password: req.body.Password,
+                Password: hashedPassword,
                 Email: req.body.Email,
                 Birthday: req.body.Birthday,
             },
